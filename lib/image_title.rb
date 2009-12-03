b8b706cd99d7dd34819841d825446660270c8bed
Spencer Steffen <Spencer@mbpro.local>
2009-12-03 13:11:39 -0800

    fixed the same file bug

--- lib/image_title.rb
+++ lib/image_title.rb
@@ -6,19 +6,17 @@ class ImageTitle < ActiveRecord::Base
 
   before_destroy :delete_current_image
  
-  def say(text="",options={})
+  def say(text="",options={})    
     @options = options    
  
     @text = text.gsub(/'/, "\`")
-     
-    logger.info "[has_image_title] text will be: #{@text}"
-     
+
     @options[:command_path] = clean_path( @options[:command_path] )
     @options[:font] = clean_path( @options[:font] )
     @options[:font_path] = clean_path( @options[:font_path] )
     @options[:destination] = clean_path( @options[:destination] )
 
-    if self.file_name
+    if has_current_file?
       backup_current_image
       delete_current_image
     end
@@ -32,16 +30,19 @@ class ImageTitle < ActiveRecord::Base
     
     info = `identify -format "%b,%w,%h" #{@options[:destination]}/#{@filename}`.split(",")
     
-    if info.empty? || !File.exists?(@filename)
-      err = "[has_image_title] Image generation failed - using backup image if it exists. Please make sure ImageMagick is installed properly."
+    logger.info "[has_image_title] #{info.inspect}"
+    
+    if info.empty? || !File.exists?("#{@options[:destination]}/#{@filename}")
+      msg = "Image generation failed - using backup image if it exists. Please make sure ImageMagick is installed properly."
+      err = "[has_image_title] #{msg}"
       logger.error err
-      restore_backup_image
-      self.errors.add_to_base("Image generation failed - using backup image if it exists. Please make sure ImageMagick is installed properly.")
+      restore_backup_image if has_current_file?
+      self.errors.add_to_base(msg)
     else  
       self.file_name = @filename
-      self.file_size = info[0]
-      self.width = info[1]
-      self.height = info[2]
+      self.file_size = info[0].to_i
+      self.width = info[1].to_i
+      self.height = info[2].to_i
       self.save
     end
   end
@@ -72,7 +73,7 @@ class ImageTitle < ActiveRecord::Base
   
   def backup_current_image
     @options ||= self.imagable.options
-    return false unless @options    
+    return false unless @options || !has_current_file?
     setup_backup_folder
     path = "#{@options[:destination]}/#{self.file_name}"
     new_path = "#{@options[:destination]}/backups/#{self.file_name}"
@@ -81,7 +82,7 @@ class ImageTitle < ActiveRecord::Base
   
   def restore_backup_image
     @options ||= self.imagable.options
-    return false unless @options    
+    return false unless @options || !has_current_file?
     path = "#{@options[:destination]}/backups/#{self.file_name}"
     new_path = "#{@options[:destination]}/#{self.file_name}"
     FileUtils.cp(path, new_path) if !path.empty? && File.exists?(path)
@@ -94,12 +95,20 @@ class ImageTitle < ActiveRecord::Base
     File.delete(path) if !path.empty? && File.exists?(path)
   end
   
+  def has_current_file?
+    return self.file_name && (!self.file_name.nil? || self.file_name.empty?)
+  end
+  
   def clean_path(str="")
     str.to_s.gsub(/\/$/, "")
   end
+  
+  def convert_command
+    @options[:command_path] && !@options[:command_path].empty? ? "#{@options[:command_path]}/convert" : "convert"
+  end
        
   def title_command
-    "#{@options[:command_path]}/convert \
+    "#{convert_command} \
     -trim \
     -antialias \
     -background '#{@options[:background_color]}#{@options[:background_alpha]}' \
@@ -112,6 +121,7 @@ class ImageTitle < ActiveRecord::Base
     caption:'#{@text}' \
     #{@options[:destination]}/#{@filename}".gsub(/\\/, '').gsub(/\s{1,}/, ' ')
   end
+  
         
   def logger
     Citrus::HasImageTitle::logger
